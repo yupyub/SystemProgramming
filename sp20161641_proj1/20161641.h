730130269c85a4f2b5665834d5fc24afbaec117e
SungYubJung <steve5003@naver.com>
2020-03-29 18:22:08 +0900

    ,

--- sp20161641_proj1/20161641.h
+++ sp20161641_proj1/20161641.h
@@ -2,6 +2,7 @@
 #include <dirent.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 typedef struct _instNode{
 	char str[100];
 	int caseNum;
@@ -11,7 +12,9 @@ typedef struct _instNode{
 }instNode;
 instNode* newInstNode(); // 빈 노드를 반환 
 void instSetInit(); // 명령어 종류를 읽어들여, 명령어 리스트를 만든다 
-int getInstruction(); // 명령어를 읽어들인다 
-instNode* classifyInstruction(char[]); // 명령어를 분류 
-void storeInstruction(instNode*); // 규칙에 맞는 명령어 저장
+void loadOneInstruction(FILE*, instNode**); // 재귀적으로 명령어 리스트 생성 
+int getInstruction(); // bash에서 명령어를 읽어들인다 
+void parser(char[], int*, char[100][100]); // 문자열을 파싱
+int classifyInstruction(int , char[100][100]); // 명령어를 분류 
+void storeInstruction(char[]);// 규칙에 맞는 명령어 저장
 void help(); // 도움말 출력 
--- sp20161641_proj1/InstructionProcessing.c
+++ sp20161641_proj1/InstructionProcessing.c
@@ -1,5 +1,4 @@
 #include "20161641.h"
-char str[100]; // 명령어를 읽어들일 char 배열 
 instNode* instSet = NULL;
 instNode* instHistory = NULL;
 
@@ -10,131 +9,62 @@ instNode* newInstNode(){ // 빈 노드를 반환
 } 
 void instSetInit(){ // 명령어 종류를 읽어들여, 명령어 리스트를 만든다 
 	FILE *fp = fopen("operations.txt","r");
-	instSet = newInstNode();
-	fscanf(fp,"%s",instSet->str);
-	fscanf(fp,"%d",&(instSet->caseNum));
-	fscanf(fp,"%d",&(instSet->valNum));
-	instSet->link = NULL;
-	instNode* newNode = newInstNode();
-	while(fscanf(fp,"%s",newNode->str) != EOF){
-		fscanf(fp,"%d",&(newNode->caseNum));
-		fscanf(fp,"%d",&(newNode->valNum));
-		newNode->link = NULL;
-		instNode* temp = instSet;
-		while(temp->link != NULL)
-			temp = temp->link;
-		temp->link = newNode;
-		newNode = newInstNode();
-	}
+	loadOneInstruction(fp,&instSet);
 	fclose(fp);
 	return;
 }
-int getInstruction(){ // 명령어를 읽어들인다 
-	scanf("%[^\n]%*c",str);
-	instNode* tmpInst = classifyInstruction(str);
+void loadOneInstruction(FILE *fp,instNode **instSet){ // 재귀적으로 명령어 리스트 생성
+	char str[100];
+	if(fscanf(fp,"%s",str)==EOF)
+		return;
+	*instSet=newInstNode();
+	strcpy((*instSet)->str,str);
+	fscanf(fp,"%d",&(*instSet)->caseNum);
+	fscanf(fp,"%d",&(*instSet)->valNum);
+	(*instSet)->link=NULL;
+	loadOneInstruction(fp,&(*instSet)->link);
+}
 
-	/*
-	////////////////////////////////////////
-	printf("=====\n%s\n",tmpInst->str);
-	printf("caseNum : %d\n",tmpInst->caseNum);
-	printf("valNum : %d\n",tmpInst->valNum);
-	printf("val : ");
-	for(int i = 0;i<10;i++){
-		printf("%d ",tmpInst->val[i]);
+int getInstruction(){ // bash에서 명령어를 읽어들인다 
+	char input[100]; // 명령어를 읽어들일 char 배열 
+	int argc;
+	char argv[100][100];
+	printf("sicsim> ");
+	fgets(input,100,stdin);
+	//scanf("%[^\n]%*c",input);
+	parser(input,&argc,argv);
+	printf("argc: %d\n", argc);
+	for(int i = 0;i<argc+3;i++){
+		printf(">> %s\n",argv[i]);
 	}
-	printf("\n");
-	////////////////////////////////////////
-	*/
+	/*
+	 = classifyInstruction(argc,argv);
+
 
 	if(tmpInst->caseNum == -1)
 		return 0;
-	storeInstruction(tmpInst);
+	//storeInstruction(tmpInst);
 	switch(tmpInst->caseNum){
 		case 2: 
 			return 1;
 	}
+	*/
 	return 0;
 }
-instNode* classifyInstruction(char str[]){ // 명령어를 분류 
-	instNode* retNode = newInstNode();
-	int idx = 0;
-	int strLen = strlen(str);
-	while(idx<strLen){
-		if('a'<=str[idx] && str[idx]<='z')
-			retNode->str[idx] = str[idx];
-		else
-			break;
-		idx++;
+void parser(char str[], int* argc, char argv[100][100]){
+	char sep[] = ", \n";
+	char* token;
+	*argc = 0;
+	token = strtok(str,sep);
+	while(token != NULL){
+		strcpy(argv[(*argc)++],token);
+		token = strtok(NULL, sep);
 	}
-	//printf("Pass0\n");
-	int opValNum = -1;
-	instNode* tempNode = instSet;
-	retNode->str[idx] = 0;
-	while(1){
-		if(strcmp(retNode->str,tempNode->str) == 0){
-			opValNum = tempNode->valNum;
-			retNode->caseNum = tempNode->caseNum;
-			break;
-		}
-		if(tempNode -> link == NULL)
-			break;
-		tempNode = tempNode->link;
-	}
-	if(opValNum == -1){
-		retNode->caseNum = -1;
-		return retNode;
-	}
-	//printf("Pass1\n");
-	int numCounter = 0;
-	int tmpNum = -1;
-	while(numCounter < opValNum && idx<strLen){
-		tmpNum = -1;
-		while(idx<strLen){
-			if('0'<=str[idx] && str[idx]<='9'){
-				if(tmpNum == -1)
-					tmpNum = 0;
-				tmpNum*=10;
-				tmpNum += str[idx] - '0';
-				idx++;
-			}
-			else if(str[idx] == ',' && tmpNum != -1){
-				retNode->val[numCounter] = tmpNum;
-				numCounter++;
-				idx++;
-				break;
-			}
-			else if(str[idx] == ' '){
-				idx++;
-			}
-			else{
-				opValNum = -1;
-				idx++;
-				break;
-			}
-		}
-	}
-	if(opValNum == -1){
-		retNode->caseNum = -1;
-		return retNode;
-	}
-	if(tmpNum != -1){
-		retNode->val[numCounter] = tmpNum;
-		numCounter++;
-	}
-	//printf("Pass2\n");
-	while(idx< strLen){
-		if(str[idx] != ' '){
-			opValNum = -1;
-			break;
-		}
-		idx++;
-	}
-	if(opValNum == -1)
-		retNode->caseNum = -1;
-	retNode->valNum = numCounter;
-	return retNode;
+	return;
 }
-void storeInstruction(instNode* inst){ // 규칙에 맞는 명령어 저장
+int classifyInstruction(int argv, char argc[100][100]){ // 명령어를 분류 
+	return 0;
+}
+void storeInstruction(char str[]){ // 규칙에 맞는 명령어 저장
 
 }
-
